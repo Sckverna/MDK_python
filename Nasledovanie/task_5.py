9cff89ae9063199784f8b1c830e193371768cf38
Sckverna <122283832+Sckverna@users.noreply.github.com>
2023-04-20 22:00:25 +0300

    Revert "add_nasledovanie"
    This reverts commit a7952b7d9dd51aeca208d8fcb46ac9471aff6290.

--- Nasledovanie/task_5.py
+++
@@ -1,8 +0,0 @@
-from time import time, localtime
-class Clock:
-    @staticmethod
-    def Say_time():
-        time()
-        rez = localtime()
-        print(f"{rez.tm_hour} : {rez.tm_min} : {rez.tm_sec}")
-Clock.Say_time()
